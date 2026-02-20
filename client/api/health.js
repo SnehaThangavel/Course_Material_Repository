b60575f945a7aa8b2fd8543829598f425c25febf
SnehaThangavel <snehathangavel184@gmail.com>
2026-02-20 14:15:52 +0530

    Fix: ESM export for health check

--- client/api/health.js
+++ client/api/health.js
@@ -1,6 +1,6 @@
-module.exports = (req, res) => {
+export default function handler(req, res) {
     res.status(200).json({
-        message: 'Client-folder API reached!',
+        message: 'Client-folder API reached (ESM)!',
         time: new Date().toISOString()
     });
-};
+}
