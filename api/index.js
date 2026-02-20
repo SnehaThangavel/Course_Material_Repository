667614db831fc5c9339d5edf992bbad92744e15b
SnehaThangavel <snehathangavel184@gmail.com>
2026-02-20 15:34:45 +0530

    Diag: Simplify root api/index.js to basic JSON

--- api/index.js
+++ api/index.js
@@ -1,15 +1,3 @@
-const app = require('../client/server/server.cjs');
-const connectDB = require('../client/server/config/db.cjs');
-
-module.exports = async (req, res) => {
-    try {
-        await connectDB();
-        return app(req, res);
-    } catch (error) {
-        console.error('Production Serverless Error:', error);
-        res.status(500).json({
-            message: 'Internal Server Error',
-            error: error.message
-        });
-    }
+module.exports = (req, res) => {
+    res.status(200).json({ message: "Root API Detection Successful", time: new Date() });
 };
