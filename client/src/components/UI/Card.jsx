d1c8446aa7ae0ff467a67bc81bfccaa2a576ebb5
SnehaThangavel <snehathangavel184@gmail.com>
2026-02-20 15:16:59 +0530

    Fix: Re-standardize client-root deployment with corrected relative paths

--- client/src/components/UI/Card.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../../styles/components.css';
-
-const Card = ({ children, className = '' }) => {
-    return <div className={`card ${className}`}>{children}</div>;
-};
-
-export default Card;
