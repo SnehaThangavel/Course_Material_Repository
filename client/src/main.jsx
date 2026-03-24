54cd35ef25c8a900fa034af9dfd044ab6b22594d
SnehaThangavel <snehathangavel184@gmail.com>
2026-03-24 10:01:04 +0530

    Configure axios URL and add Render _redirects

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,7 @@ import { AuthProvider } from './context/AuthContext'
 import axios from 'axios'
 
 axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
